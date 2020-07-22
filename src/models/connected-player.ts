bbc5a2c92cfb4173725c9fedbedcf92cb35529b6
Joshua Zhang <toruta39@gmail.com>
2020-07-22 11:32:33 +0900

    update doc generation

--- src/models/connected-player.ts
+++ src/models/connected-player.ts
@@ -2,7 +2,6 @@ import Player from './player';
 
 /**
  * Representing a connected player.
- * @protected
  * @extends Player
  */
 export default class ConnectedPlayer extends Player {}
--- src/models/context-player.ts
+++ src/models/context-player.ts
@@ -2,7 +2,6 @@ import Player from './player';
 
 /**
  * Representing a context player.
- * @protected
  * @extends Player
  */
 export default class ContextPlayer extends Player { }
