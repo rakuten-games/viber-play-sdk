2e32bc228786ae388b6060d457f4862c4f3ddb75
Joshua Zhang <toruta39@gmail.com>
2020-08-11 17:25:28 +0900

    open browser automatically

--- scripts/start-examples.sh
+++ scripts/start-examples.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+if [[ $OSTYPE == "darwin"* ]]; then
+  sleep 1 && open https://localhost:8080/examples/ &>/dev/null
+elif command -v xdg-open &> /dev/null; then
+  sleep 1 && xdg-open https://localhost:8080/examples/ &>/dev/null
+fi
+
 echo 'Serving examples at https://localhost:8080/examples/'
 caddy run
+
