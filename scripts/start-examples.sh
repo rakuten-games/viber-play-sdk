e7a1afe76cc230820b22805ddce6d3a4ae3a22a2
Joshua Zhang <toruta39@gmail.com>
2020-08-11 17:30:07 +0900

    revise shell script

--- scripts/start-examples.sh
+++ scripts/start-examples.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $OSTYPE == "darwin"* ]]; then
-  sleep 1 && open https://localhost:8080/examples/ &>/dev/null
+  (sleep 1 && open https://localhost:8080/examples/ &>/dev/null) &
 elif command -v xdg-open &> /dev/null; then
-  sleep 1 && xdg-open https://localhost:8080/examples/ &>/dev/null
+  (sleep 1 && xdg-open https://localhost:8080/examples/ &>/dev/null) &
 fi
 
 echo 'Serving examples at https://localhost:8080/examples/'
