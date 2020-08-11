95162df9d171cf1e3d9443089b0ead04f92edcd1
Joshua Zhang <toruta39@gmail.com>
2020-08-11 17:05:16 +0900

    use caddy as static server
    due to ecstatic is deprecated

--- scripts/start-examples.sh
+++ scripts/start-examples.sh
@@ -1,18 +1,2 @@
-echo 'Checking SSL cert...'
-
-if [ -e cert.pem ] && [ -e key.pem ]; then
-  echo 'Cert found.'
-else
-  echo 'Cert not found.'
-  echo 'Creating SSL for local development...'
-
-  rm cert.pem key.pem
-
-  openssl genrsa 2048 > key.pem
-  openssl req -x509 -days 1000 -new -key key.pem -out cert.pem
-
-  echo 'Cert created.'
-fi
-
-echo 'Starting static server...'
-http-server --ssl -c-1
+echo 'Serving examples at https://localhost:8080/examples/'
+caddy run
